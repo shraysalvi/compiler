669390305f54966265933f7f65e5dd81de983c4e
SHRAY SALVI <80808215+shraysalvi@users.noreply.github.com>
2022-08-22 18:49:26 +0530

    Update views.py

--- file_explorer/views.py
+++ file_explorer/views.py
@@ -67,7 +67,7 @@ def run_code(request):
     code = request.GET.get('code')
     with open("codes/code.py", "w") as file:
         file.write(code)
-    cmd = ['script.sh']  
+    cmd = ['bash script.sh']  
 #     cmd = ["python3", "codes/code.py"]
     sp = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, 
                             stdout=subprocess.PIPE, stderr=subprocess.PIPE,
